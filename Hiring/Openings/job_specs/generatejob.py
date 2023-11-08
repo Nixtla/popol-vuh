f63d2b35f3198f2dbda9af2803208c63b9eae907
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-11-08 16:32:08 +0000

    feat: update structure to allow multiple files

--- Hiring/Openings/job_specs/generatejob.py
+++ Hiring/Openings/job_specs/generatejob.py
@@ -1,5 +1,6 @@
+import argparse
 import re
-import sys
+from typing import List
 
 
 def load_definitions(file_paths):
@@ -8,23 +9,24 @@ def load_definitions(file_paths):
     # Variables to keep track of the current definition and its content
     current_def = None
     current_text = []
-    
+
     def save_definition():
         # If a definition is being tracked, save it to the dictionary
         if current_def and current_text:
-            definitions[current_def] = '\n'.join(current_text).strip()
-    
+            definitions[current_def] = "\n".join(current_text).strip()
+
     # Process each file path provided
     for file_path in file_paths:
-        with open(file_path, 'r') as f:
+        print(f"Loading definitions from {file_path}")
+        with open(file_path, "r") as f:
             # Read file line by line
             for line in f:
                 # Ignore HTML comment lines
-                if re.match(r'^\s*<!--.*-->\s*$', line):
+                if re.match(r"^\s*<!--.*-->\s*$", line):
                     continue
-                
+
                 # Look for lines that define a new definition
-                match = re.match(r'^\s*Def (\w+):\s*(.*)', line)
+                match = re.match(r"^\s*Def (\w+):\s*(.*)", line)
                 if match:
                     # Save any ongoing definition to the dictionary
                     save_definition()
@@ -34,18 +36,17 @@ def load_definitions(file_paths):
                 elif current_def:
                     # If we're inside a definition, append line content
                     current_text.append(line.rstrip())
-                    
+
         # After reading a file, save the last definition
         save_definition()
-    
+
     return definitions
 
-def preprocess_markdown(output_file, definitions):
+
+def preprocess_markdown(definitions):
     # Markdown template with placeholders for definitions
     template = """
 # {{Def Title}}
-
-**Salary**: {{Def Salary}}. | **Equity**: {{Def Equity}}.
 
 **Location**: Remote. | **Job Type**: Full-time.
 
@@ -81,31 +82,28 @@ def preprocess_markdown(output_file, definitions):
         template = template.replace(placeholder, replacement)
 
     # Write the processed content to the output file
-    with open(output_file, 'w') as f:
+    output_file = definitions["{{Def OutputFile}}"]
+    with open(output_file, "w") as f:
         # Write a comment indicating the file is auto-generated at the top
         f.write("<!-- Auto Generated file, do not modify -->")
         f.write(template)
 
-DEFINITIONS_FILE_PATH = '../../../GeneralDefinitions.md'
 
-def main():
-    # Check if the correct number of arguments are provided, if not print usage information
-    if len(sys.argv) < 2:
-        print("Usage: generatejob.py job_specs.md job_description.md")
-        sys.exit(1)
+DEFINITIONS_FILE_PATH = "../../../GeneralDefinitions.md"
 
-    # Collect definition files from arguments
-    definitions_files = sys.argv[1:-1]
+
+def main(files: List[str]):
     # Always read the hardcoded definitions file in addition to those passed as arguments
-    definitions_files.insert(0, DEFINITIONS_FILE_PATH)  # Add the hardcoded file path at the beginning
-    output_file = sys.argv[-1]
-
+    definitions_files = [DEFINITIONS_FILE_PATH] + files
     # Load definitions from all provided definition files and the hardcoded file
     definitions = load_definitions(definitions_files)
-
     # Preprocess with the loaded definitions and output the result
-    preprocess_markdown(output_file, definitions)
+    preprocess_markdown(definitions)
+
 
 # Entry point of the script
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Generate Job Posting")
+    parser.add_argument("--files", nargs="+", help="List of files to load")
+    args = parser.parse_args()
+    main(args.files)
